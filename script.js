f2fef0a360f1778d04a9fad565eabb4ece5f8ca7
maliacho <maliacho21@gmail.com>
2023-08-23 16:42:25 -0700

    moved js link in HTML file to head. added psuedocode to JS file.

--- script.js
+++ script.js
@@ -12,3 +12,11 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//when user clicks 'generate password' button a prompt appears asking the following
+  //how many characters to contain (min & max)
+  //any special characters
+  //any numbers
+  //any lowercase
+  //any uppercase
+//display password in textbox
