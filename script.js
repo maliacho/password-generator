8235bc0aec138bd237172c6b6b98b0eb2a603edc
maliacho <maliacho21@gmail.com>
2023-08-24 16:40:48 -0700

    updated generatePassword function psuedocode. rewrote parts of the function- but not complete

--- script.js
+++ script.js
@@ -5,20 +5,41 @@ let generateBtn = document.querySelector("#generate");
 // const passwordCriteria = {
 //   lowers: "qwetyuiopasdfghjklzxcvbnm",
 //   uppers: "QWERTYUIOPASDFGHJKLZXCVBNM",
-//   special: "!@#$%&?",
-//   numbers: "123456789",
+//   special: "!@#$%&?<>/\=+-",
+//   numbers: "0123456789",
 // }
 
 function generatePassword () {
-  const lowers = "qwetyuiopasdfghjklzxcvbnm";
-  const uppers = "QWERTYUIOPASDFGHJKLZXCVBNM";
-  const special = "!@#$%&?";
-  const numbers = "123456789";
-  confirm("Would you like to include lowercase letters?");
-  confirm("Would you like to include uppercase letters?");
-  confirm("Would you like to include special characters?");
-  confirm("Would you like to include numbers?");
+ 
+  // Allowed items for password creation
+    let lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    let upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    let characters = ["!", "@", "#", "$", "%", "^", "&", "*", "?"];
+    let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+    // empty array to store user input
+    let userResults = [];
+
+  // Prompt the user for the password criteria
+    // password length (8-128)
+    let userLength = prompt("How long would you like your password?")
+    // lowercase, uppercase, numbers, special characters
+    let userLower = confirm("Would you like to include lowercase letters?")
+    let userUpper = confirm("Would you like to include uppercase letters?")
+    let userCharacters = confirm("Would you like to include special characters?")
+    let userNumbers = confirm("Would you like to include numbers?")
+
+  // Validate input
+    // if (userLength < "8" && userLength > "128") {
+    //   prompt("Please enter a number between 8 and 128")
+    // }
+
+  //Generate password
+
+  // Display password in the textbox
+  // return "Generated Password will go here!"
 }
+
 
 
 // Write password to the #password input
@@ -33,13 +54,4 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// ---------------------------------------------------
-// PSUEDOCODE 
-//when user clicks 'generate password' button a prompt appears asking the following
-  //how many characters to contain (min & max)
-  //any special characters
-  //any numbers
-  //any lowercase
-  //any uppercase
-//display password in textbox
 
